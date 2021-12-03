582b1239d27e1d94b985377fe45090050c55db35
tkeeler777 <360286177@qq.com>
2021-12-03 13:39:43 +0800

    fix(tenant):create & list api

--- pkg/kubernetes/tenant.go
+++ pkg/kubernetes/tenant.go
@@ -3,103 +3,111 @@ package kubernetes
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/util/net"
 	k8s "k8s.io/client-go/kubernetes"
 )
 
-func TenantCreate(tenantTitle string) (*TenantCreateResp, error) {
-	clientset, err := Client()
+func TenantCreate(title, adminName, adminPW string) error {
+	if len(title) == 0 {
+		return errors.New("title param nil")
+	}
+	client, err := Client()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	namespace, err := GetTKeelNameSpace(clientset)
-	if err != nil {
-		return nil, err
+	tenant := &TenantCreateIn{Title: title}
+	if len(adminName) != 0 && len(adminPW) != 0 {
+		admin := TenantAdmin{UserName: adminName, Password: adminPW}
+		tenant.Admin = admin
 	}
-	return CreateTenant(clientset, namespace, tenantTitle)
+
+	return CreateTenant(client, tenant)
 }
 
-func CreateTenant(client k8s.Interface, namespace, tenantTitle string) (*TenantCreateResp, error) {
-	res := client.CoreV1().RESTClient().Post().
-		Namespace(namespace).
-		Resource("services").
-		SubResource("proxy").
-		Name(net.JoinSchemeNamePort("", "keel", "8080")).
-		Suffix("auth/tenant/create").
-		Body([]byte(fmt.Sprintf(`{"title":"%s"}`, tenantTitle)))
+func CreateTenant(client k8s.Interface, tenant *TenantCreateIn) error {
+	rudder, err := AppPod(client, "rudder")
+	if err != nil {
+		return err
+	}
+	body, _ := json.Marshal(tenant)
+	res := rudder.App().Request(client.CoreV1().RESTClient().Post()).
+		Suffix("v1/tenants").Body(body)
+
 	ret := res.Do(context.TODO())
+	if ret.Error() != nil {
+		return fmt.Errorf("k8s query result err: %w", ret.Error())
+	}
+	_, err = ret.Raw()
+	if err != nil {
+		return fmt.Errorf("do k8s query err: %w", err)
+	}
+
+	return nil
+}
+
+func TenantList() ([]Tenant, error) {
+	client, err := Client()
+	if err != nil {
+		return nil, err
+	}
+	rudder, err := AppPod(client, "rudder")
+	if err != nil {
+		return nil, err
+	}
+	res := rudder.App().Request(client.CoreV1().RESTClient().Get()).
+		Suffix("v1/tenants")
+
+	ret := res.Do(context.TODO())
+	if ret.Error() != nil {
+		return nil, fmt.Errorf("k8s query result err: %w", err)
+	}
 	raw, err := ret.Raw()
 	if err != nil {
 		return nil, fmt.Errorf("do k8s query err: %w", err)
 	}
-	resp := TenantCreateResponse{}
-	err = json.Unmarshal(raw, &resp)
-	return &resp.Data, fmt.Errorf("unmarshall to struct: %w", err)
-}
-
-func TenantList() (*TenantListData, error) {
-	clientset, err := Client()
-	if err != nil {
-		return nil, err
-	}
-
-	namespace, err := GetTKeelNameSpace(clientset)
-	if err != nil {
-		return nil, err
-	}
-	return ListTenant(clientset, namespace)
-}
-
-func ListTenant(client k8s.Interface, namespace string) (*TenantListData, error) {
-	res := client.CoreV1().RESTClient().Post().
-		Namespace(namespace).
-		Resource("services").
-		SubResource("proxy").
-		Name(net.JoinSchemeNamePort("", "keel", "8080")).
-		Suffix("auth/tenant/list").
-		Body([]byte(`{}`))
-	ret := res.Do(context.TODO())
-	raw, err := ret.Raw()
-	if err != nil {
-		return nil, fmt.Errorf("k8s query err:%w", err)
-	}
 	resp := TenantListResponse{}
 	err = json.Unmarshal(raw, &resp)
-	if err != nil {
-		err = fmt.Errorf("unmarshall json to struct err :%w", err)
-	}
-	return &resp.Data, err
+	return resp.Data, err
+}
+
+type TenantCreateIn struct {
+	Title string      `json:"title"`
+	Admin TenantAdmin `json:"admin"`
+}
+
+type TenantAdmin struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
 }
 
 type TenantCreateResponse struct {
-	Ret  int              `json:"ret"`
+	Code int              `json:"code"`
 	Msg  string           `json:"msg"`
 	Data TenantCreateResp `json:"data"`
 }
+
 type TenantListResponse struct {
-	Ret  int            `json:"ret"`
-	Msg  string         `json:"msg"`
-	Data TenantListData `json:"data"`
+	Ret  int      `json:"ret"`
+	Msg  string   `json:"msg"`
+	Data []Tenant `json:"data"`
 }
 
-type TenantListData struct {
-	TenantList []TenantCreateResp `json:"tenant_list"`
-}
 type TenantCreateResp struct {
-	TenantID    string `json:"tenant_id"`
-	Title       string `json:"title"`
-	CreatedTime int64  `json:"created_time"`
-	TenantAdmin User   `json:"tenant_admin"`
+	TenantID int         `json:"tenant_id"`
+	Title    string      `json:"title"`
+	Admin    TenantAdmin `json:"admin"`
 }
 
 type User struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	Password   string `json:"password"`
-	TenantID   string `json:"tenant_id"`
-	Email      string `json:"email"`
-	CreateTime int64  `json:"create_time"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Tenant struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Remark string `json:"remark"`
 }
